2720c37a36625089c64fffb80d7c58d8740fc64d
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-02-04 17:25:03 +0300

    ES module

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import posts from "./routes/posts.js";
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,34 +17,9 @@ const app = express();
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
 
-let posts = [
-  { id: 1, title: "Post One" },
-  { id: 2, title: "Post Two" },
-  { id: 3, title: "Post Three" },
-];
+//Routes
 
-// Get all posts
-app.get("/api/posts", (req, res) => {
-  const limit = parseInt(req.query.limit);
-
-  if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
-  }
-});
-
-// Get single post
-app.get("/api/posts/:id", (req, res) => {
-  //   console.log(req.params);
-  const id = parseInt(req.params.id);
-  const post = posts.find((post) => post.id === id);
-  if (!post) {
-    res.status(404).json({ msg: `A post with the id of ${id} was not found` });
-  } else {
-    res.status(200).json(post);
-  }
-});
+app.use("/api/posts", posts);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
